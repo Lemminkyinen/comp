1b026f36e6f193709a33dcd07c715796a1c5f096
Jarmo Tenhunen <jarmo@seravo.com>
2025-01-26 16:17:09 +0200

    Clean up the code

--- python_comp/src/lib.rs
+++ python_comp/src/lib.rs
@@ -1,3 +1,5 @@
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
@@ -19,6 +21,55 @@ impl Parse for Comprehension {
     }
 }
 
+impl ToTokens for Comprehension {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        // let Comprehension { mapping, for_if_clause, additional_for_if_clauses }
+        let mapping = &self.mapping;
+        let for_if_clause = &self.for_if_clause;
+        let additional_for_if_clauses = &self.additional_for_if_clauses;
+
+        let mut for_ifs = core::iter::once(for_if_clause)
+            .chain(additional_for_if_clauses.0.iter())
+            .rev();
+
+        let output = {
+            let first_if = for_ifs.next().expect("There is always one for if clause!");
+            let ForIfClause { pattern, sequence, conditions } = first_if;
+            let Conditions(conditions) = conditions;
+            quote! {
+                ::core::iter::IntoIterator::into_iter(#sequence)
+                .filter_map(|#pattern| {
+                    if true #(&& (#conditions))* { Some(#mapping) }
+                    else { None }
+                })
+            }
+        };
+
+        let res = for_ifs.fold(output, |cur_output, next_layer| {
+            let ForIfClause { pattern, sequence, conditions } = next_layer;
+            let Conditions(conditions) = conditions;
+            quote! {
+                ::core::iter::IntoIterator::into_iter(#sequence)
+                .filter_map(|#pattern| {
+                    if true #(&& (#conditions))* { Some(#cur_output) }
+                    else { None }
+                })
+                .flatten()
+            }
+        });
+
+        tokens.extend(res);
+    }
+}
+
+impl Into<TokenStream> for Comprehension {
+    fn into(self) -> TokenStream {
+        let tokens = &mut TokenStream2::new();
+        self.to_tokens(tokens);
+        quote! { #tokens }.into()
+    }
+}
+
 struct Mapping(Expr);
 
 impl Parse for Mapping {
@@ -28,7 +79,7 @@ impl Parse for Mapping {
 }
 
 impl ToTokens for Mapping {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
         self.0.to_tokens(tokens)
     }
 }
@@ -42,7 +93,7 @@ impl Parse for Pattern {
 }
 
 impl ToTokens for Pattern {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
         self.0.to_tokens(tokens)
     }
 }
@@ -57,7 +108,7 @@ impl Parse for Condition {
 }
 
 impl ToTokens for Condition {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
         self.0.to_tokens(tokens)
     }
 }
@@ -120,53 +171,20 @@ impl Parse for AdditionalForIfClauses {
 
 /// Simple python like list comprehension
 #[proc_macro]
-pub fn vec_comp(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let iter_result = proc_macro2::TokenStream::from(iter_comp(input));
+pub fn vec_comp(input: proc_macro::TokenStream) -> TokenStream {
+    let iter_result: proc_macro2::TokenStream = iter_comp(input).into();
     quote! { #iter_result.collect::<::std::vec::Vec<_>>() }.into()
 }
 
 /// Simple python like set comprehension
 #[proc_macro]
-pub fn set_comp(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let iter_result = proc_macro2::TokenStream::from(iter_comp(input));
+pub fn set_comp(input: proc_macro::TokenStream) -> TokenStream {
+    let iter_result: proc_macro2::TokenStream = iter_comp(input).into();
     quote! { #iter_result.collect::<::std::collections::HashSet<_>>() }.into()
 }
 
+/// Simple python like iterator comprehension
 #[proc_macro]
-pub fn iter_comp(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let parsed = parse_macro_input!(input as Comprehension);
-
-    let Comprehension { mapping, for_if_clause, additional_for_if_clauses } = parsed;
-
-    let mut for_ifs = core::iter::once(for_if_clause)
-        .chain(additional_for_if_clauses.0.into_iter())
-        .rev();
-
-    let output = {
-        let first_if = for_ifs.next().expect("There is always one for if clause!");
-        let ForIfClause { pattern, sequence, conditions } = first_if;
-        let Conditions(conditions) = conditions;
-        quote! {
-            ::core::iter::IntoIterator::into_iter(#sequence)
-            .filter_map(|#pattern| {
-                if true #(&& (#conditions))* { Some(#mapping) }
-                else { None }
-            })
-        }
-    };
-
-    for_ifs
-        .fold(output, |cur_output, next_layer| {
-            let ForIfClause { pattern, sequence, conditions } = next_layer;
-            let Conditions(conditions) = conditions;
-            quote! {
-                ::core::iter::IntoIterator::into_iter(#sequence)
-                .filter_map(|#pattern| {
-                    if true #(&& (#conditions))* { Some(#cur_output) }
-                    else { None }
-                })
-                .flatten()
-            }
-        })
-        .into()
+pub fn iter_comp(input: proc_macro::TokenStream) -> TokenStream {
+    parse_macro_input!(input as Comprehension).into()
 }
